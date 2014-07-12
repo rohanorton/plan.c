723efbe33cfedaac19c0d65a878c9de1cdd10611
Rohan Orton <rohan.orton@gmail.com>
2014-07-12 21:50:05 +0100

    geo2json: error if latitude/longitude outside bounds

--- rohan_code/head_first/src/07_geo2json.c
+++ rohan_code/head_first/src/07_geo2json.c
@@ -14,6 +14,15 @@ int main(int argc, const char *argv[])
         } else {
             started = 1;
         }
+        if (latitude < -90 || latitude > 90) {
+            // print error messages to standard error like so...
+            fprintf(stderr, "invalid latitude: %f\n", latitude);
+            return 2;
+        }
+        if (longitude < -180 || longitude > 180) {
+            fprintf(stderr, "invalid longitude: %f\n", longitude);
+            return 2;
+        }
         printf("{\"latitude\": %f, \"longitude\": %f, \"info\": \"%s\" }", latitude, longitude, info);
     }
     puts("\n]\n}");

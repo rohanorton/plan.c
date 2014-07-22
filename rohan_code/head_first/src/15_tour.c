dd89bcac5a93590ebaaea89f2c8a559e9b7318fe
Rohan Orton <rohan.orton@gmail.com>
2014-07-22 17:01:20 +0100

    headfirst: linked list

---
+++ rohan_code/head_first/src/15_tour.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+typedef struct island {
+    char *name;
+    char *opens;
+    char *closes;
+    struct island *next;
+} island;
+
+void display (island *start)
+{
+    island *i = start;
+    for (; i != NULL; i = i->next) {
+        printf("Name: %s is open: %s-%s\n",
+                i->name, i->opens, i->closes);
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    island amity = {"Amity", "09:00", "17:00", NULL};
+    island craggy = {"Craggy", "09:00", "17:00", NULL};
+    island isla_nublar= {"Isla Nublar", "09:00", "17:00", NULL};
+    island skull = {"Skull", "09:00", "17:00", NULL};
+    island shutter = {"Shutter", "09:00", "17:00", NULL};
+
+    amity.next = &craggy;
+    craggy.next = &isla_nublar;
+    isla_nublar.next = &skull;
+    skull.next = &shutter;
+    
+    display(&amity);
+
+    return 0;
+}
